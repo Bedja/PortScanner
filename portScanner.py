632fd92766e8d011c1ed54b460a1180495ed3d59
Michael Støier <michaelstoier@Michaels-MBP.domain_not_set.invalid>
2019-07-28 21:14:54 +0200

    Working Build

--- portScanner.py
+++ portScanner.py
@@ -1,6 +1,46 @@
 #!/usr/bin/python
 
+from socket import *
+import optparse
+from threading import *
 
-### Python3 simple Portscanner
+def connScan(tgtHost, tgtPort):
+    try:
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.connect((tgtHost, tgtPort))
+        print(f'{tgtPort} TCP open')
+    except:
+        print(f'{tgtPort} Closed')
 
+    finally:
+        sock.close()
 
+def portScan(tgtHost, tgtPorts):
+    try:
+        tgtIP = gethostbyname(tgtHost)
+    except:
+        print(f"Unknown Host {tgtHost}")
+    try:
+        tgtName = gethostbyaddr(tgtIP)
+        print(f'Scan Results For {tgtName[0]}')
+    except:
+        print(f'Scan Results For: {tgtIP}')
+    setdefaulttimeout(1)
+    for tgtPort in tgtPorts:
+        t = Thread(target=connScan, args=(tgtHost, int(tgtPort)))
+        t.start()
+
+def main():
+    parser = optparse.OptionParser('Usage of program: ' + '-H <target host> -p <target port>')
+    parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
+    parser.add_option('-p', dest='tgtPort', type='string', help='specity target ports seperated by comma')
+    (options, args) = parser.parse_args()
+    tgtHost = options.tgtHost
+    tgtPorts = str(options.tgtPort).split(',')
+    if (tgtHost == None) | (tgtPorts[0] == None):
+        print(parser.usage)
+        exit(0)
+    portScan(tgtHost, tgtPorts)
+
+if __name__ == '__main__':
+    main()
